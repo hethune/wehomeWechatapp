22f6eb9d63a200f1c43a39b92ba8523b93b23128
herryben <397283864@qq.com>
2017-07-27 14:44:24 +0800

    [FEATURE] CHANGE RANKLIST.SCORE TO HOME.SCORE IN BACK-END API /api/get_total_ranking_list AND /api/get_city_ranking_list
    __Description__:
    1.change ranklist.score to home.socre in back-end api
    /api/get_city_ranking_list and /api/get_total_ranking_list
    
    __Changes__:
    nothing

--- application/app.py
+++ application/app.py
@@ -503,7 +503,7 @@ def get_city_ranking_list():
     for item in ranks:
       l.append({
         'home_id': item.home.id,
-        'score': item.score,
+        'score': item.home.score,
         'rental_income_radio': item.home.rental_income_radio,
         'rental_radio': item.home.rental_radio,
         'increase_radio': item.home.increase_radio,
@@ -534,7 +534,7 @@ def get_total_ranking_list():
     for item in ranks:
       l.append({
         'home_id': item.home.id,
-        'score': item.score,
+        'score': item.home.score,
         'rental_radio': item.home.rental_radio,
         'increase_radio': item.home.increase_radio,
         'map_box_place_name': item.home.map_box_place_name,
