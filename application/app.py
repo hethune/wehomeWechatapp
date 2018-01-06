5c8c46ea288cc75106a0c9b77615b903a7dc6070
jerryan9999 <jerry.an@rentx.io>
2018-01-06 16:00:48 +0800

    lower rpm

--- application/app.py
+++ application/app.py
@@ -17,7 +17,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 limiter = Limiter(app,key_func=get_remote_address)
-limiter.limit("100000/day;20000/hour;3000/minute;500/second")(company_blueprint)
+limiter.limit("100000/day;20000/hour;1000/minute;500/second")(company_blueprint)
 limiter.exempt(mob_blueprint)
 
 app.register_blueprint(mob_blueprint, url_prefix='/api/mob')
